73dfbc486cf11e97f4b4a2ddfcfd81569cc9fc7e
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-07-01 14:16:17 +0200

    add search fields for category admin panel

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -4,6 +4,7 @@ from .models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
+    search_fields = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
 
 
