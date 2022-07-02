5954a9b7f12ccbd73fb4cc66b509e19320aa9247
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-07-02 13:12:05 +0200

    ➕ add django-extensions utility

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd-party
+    'django_extensions',
+
+    # My apps
     'shop.apps.ShopConfig', 
 ]
 
