961eb41eb4f3bed2582e221a76a06c24ee26ca9e
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-07-02 13:14:42 +0200

    add get_absolute_url for object

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,8 +1,6 @@
-from enum import unique
-from unicodedata import category
 from django.db import models
+from django.urls import reverse
 
-# Catogery model with name - slug and Meta class for ordering
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(unique=True)
@@ -17,6 +15,8 @@ class Category(models.Model):
 
     # the convention to retrieve the URL for a given object
     def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
@@ -24,14 +24,17 @@ class Product(models.Model):
     slug = models.SlugField(max_length=255, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # DecimalField instead of FloatField to avoid rounding issues.
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('name',)
-        index_together = (('id', 'slug'),)
+        index_together = (('id', 'slug'),) # plan to query products by both id and slug
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
