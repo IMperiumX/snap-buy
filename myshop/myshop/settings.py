e3779d09984ee3c27285ef68e537db4031398563
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-07-03 23:12:23 +0200

    Storing shopping carts in sessions

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -119,6 +119,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Cart session
+CART_SESSION_ID = "cart"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
