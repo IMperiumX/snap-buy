d65f04d46b89f8bb83152352e46085687ac1fa51
Restyled.io <commits@restyled.io>
2022-07-04 12:06:38 +0000

    Restyled by black

--- myshop/orders/forms.py
+++ myshop/orders/forms.py
@@ -5,5 +5,4 @@ from .models import Order
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ["first_name", "last_name", "email",
-                  "address", "postal_code", "city"]
+        fields = ["first_name", "last_name", "email", "address", "postal_code", "city"]
