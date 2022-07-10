512c0fff7e40593cac8c348598d8385906e284ef
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-07-10 15:26:09 +0200

    Applying coupons to order

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,3 +1,7 @@
+from decimal import Decimal
+
+from coupons.models import Coupon
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from shop.models import Product
 
@@ -13,25 +17,30 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     braintree_id = models.CharField(max_length=150, blank=True)
+    coupon = models.ForeignKey(
+        Coupon, related_name="orders", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    discount = models.IntegerField(
+        default=0, validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
 
     class Meta:
-        ordering = ("-created", )
+        ordering = ("-created",)
 
     def __str__(self):
         return f"Order {self.id}"
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost - total_cost * (self.discount / Decimal(100))
 
 
 # Create your models here.
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order,
-                              related_name="items",
-                              on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,
-                                related_name="order_items",
-                                on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, related_name="order_items", on_delete=models.CASCADE
+    )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
