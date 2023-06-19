cd835151f7b536ca06d0aa461a71d348e20c9d31
y8l <yusufadell.dev@gmail.com>
2023-06-19 14:59:00 +0300

    feat(paymen): intialize payment model

--- snap_buy/payment/models.py
+++ snap_buy/payment/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Payment(models.Model):
+    payment_type = models.CharField(max_length=255)
+    allowed = models.BooleanField(default=True)
