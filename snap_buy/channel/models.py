9a1c60bf54920e0e7f07f406c9ed727c4484defb
y8l <yusufadell.dev@gmail.com>
2023-06-19 15:05:39 +0300

    chore(channel): add chennel model

---
+++ snap_buy/channel/models.py
@@ -0,0 +1,42 @@
+from django.conf import settings
+from django.db import models
+from django_countries.fields import CountryField
+
+from . import AllocationStrategy, MarkAsPaidStrategy, TransactionFlowStrategy
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=250)
+    is_active = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=255, unique=True)
+    currency_code = models.CharField(max_length=settings.DEFAULT_CURRENCY_CODE_LENGTH)
+    default_country = CountryField()
+    allocation_strategy = models.CharField(
+        max_length=255,
+        choices=AllocationStrategy.CHOICES,
+        default=AllocationStrategy.PRIORITIZE_SORTING_ORDER,
+    )
+    order_mark_as_paid_strategy = models.CharField(
+        max_length=255,
+        choices=MarkAsPaidStrategy.CHOICES,
+        default=MarkAsPaidStrategy.PAYMENT_FLOW,
+    )
+
+    default_transaction_flow_strategy = models.CharField(
+        max_length=255,
+        choices=TransactionFlowStrategy.CHOICES,
+        default=TransactionFlowStrategy.CHARGE,
+    )
+
+    automatically_confirm_all_new_orders = models.BooleanField(default=True, null=True)
+    automatically_fulfill_non_shippable_gift_card = models.BooleanField(
+        default=True,
+        null=True,
+    )
+    expire_orders_after = models.IntegerField(default=None, null=True, blank=True)
+
+    class Meta:
+        ordering = ("slug",)
+
+    def __str__(self):
+        return self.slug
