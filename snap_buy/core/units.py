58eca86f45b0e268b49066def86a6b361bb79ad4
y8l <yusufadell.dev@gmail.com>
2023-06-19 15:07:54 +0300

    chore: add weigh units

---
+++ snap_buy/core/units.py
@@ -0,0 +1,14 @@
+class WeightUnits:
+    G = "g"
+    LB = "lb"
+    OZ = "oz"
+    KG = "kg"
+    TONNE = "tonne"
+
+    CHOICES = [
+        (G, "Gram"),
+        (LB, "Pound"),
+        (OZ, "Ounce"),
+        (KG, "kg"),
+        (TONNE, "Tonne"),
+    ]
