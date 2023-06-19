472230874a447bd564b16bf9ea946d00f79f133b
y8l <yusufadell.dev@gmail.com>
2023-06-19 14:58:19 +0300

    revert: order models and user admin

--- snap_buy/orders/models.py
+++ snap_buy/orders/models.py
@@ -1,45 +1,57 @@
-from decimal import Decimal
-
-from snap_buy.coupons.models import Coupon
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from snap_buy.products.models import Product
 
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField(default=False)
-    braintree_id = models.CharField(max_length=150, blank=True)
-    coupon = models.ForeignKey(Coupon, related_name="orders", null=True, blank=True, on_delete=models.SET_NULL)
-    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
-
-    class Meta:
-        ordering = ("-created",)
-
-    def __str__(self):
-        return f"Order {self.id}"
-
-    def get_total_cost(self):
-        total_cost = sum(item.get_cost() for item in self.items.all())
-        return total_cost - total_cost * (self.discount / Decimal(100))
+    ...
 
 
 # Create your models here.
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name="order_items", on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    total = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+
+    fullfilled = models.BooleanField(default=False)
+    ship_date = models.DateTimeField(null=True, blank=True)
+    bill_date = models.DateTimeField(null=True, blank=True)
+
+    order = models.ForeignKey("Order", related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey("products.Product", related_name="order_items", on_delete=models.CASCADE)
+    size = models.ForeignKey("Size", null=True, blank=True, on_delete=models.CASCADE)
+    color = models.ForeignKey("Color", null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.id)
 
     def get_cost(self):
         return self.price * self.quantity
+
+
+class Size(models.Model):
+    size = models.CharField(max_length=254, null=True, blank=True)
+
+
+class Color(models.Model):
+    color = models.CharField(max_length=254, null=True, blank=True)
+
+
+class Promotion(models.Model):
+    code = models.CharField(max_length=24, unique=True)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+
+    percent_discount = models.FloatField(default=0)
+    discount_amount = models.IntegerField(default=0)
+
+    valid_from = models.DateField()
+    valid_to = models.DateField()
+
+    usage_limit = models.IntegerField(default=10000)
+    used_times = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.code
