d9fd44ba1a0ed53457985db11c05c2125a65d247
IMperiumX <yusufadell.dev@gmail.com>
2024-04-25 19:07:12 +0200

    Refactor imports in admin.py and apps.py

--- snap_buy/products/admin.py
+++ snap_buy/products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import ProductCategory, Product
+from .models import Product
+from .models import ProductCategory
 
 
 @admin.register(ProductCategory)
--- snap_buy/products/apps.py
+++ snap_buy/products/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ProductsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'snap_buy.products'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "snap_buy.products"
