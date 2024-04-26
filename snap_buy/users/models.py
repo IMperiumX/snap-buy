d1023548cb0e39ef2e760b5657754c9948d2aa82
IMperiumX <yusufadell.dev@gmail.com>
2024-04-26 04:36:21 +0300

    Add PhoneNumber and Profile models to users app

--- snap_buy/users/models.py
+++ snap_buy/users/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
 
 from .managers import UserManager
 
@@ -62,3 +63,39 @@ class Address(models.Model):
 
     def __str__(self):
         return self.user.get_full_name()
+
+
+class PhoneNumber(models.Model):
+    user = models.OneToOneField(User, related_name="phone", on_delete=models.CASCADE)
+    phone_number = PhoneNumberField(unique=True)
+    security_code = models.CharField(max_length=120)
+    is_verified = models.BooleanField(default=False)
+    sent = models.DateTimeField(null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return self.phone_number.as_e164
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        "users.User",
+        related_name="profile",
+        on_delete=models.CASCADE,
+    )
+    avatar = models.ImageField(upload_to="avatar", blank=True)
+    bio = models.CharField(max_length=200, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return f"{self.user.get_full_name()}"
