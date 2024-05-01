52a141d592038f25db3081f42ca8ac6397987198
IMperiumX <yusufadell.dev@gmail.com>
2024-05-01 15:21:39 +0300

    Add api routers

--- config/api_router.py
+++ config/api_router.py
@@ -4,17 +4,26 @@ from rest_framework.routers import SimpleRouter
 
 from snap_buy.orders.api.views import OrderItemViewSet
 from snap_buy.orders.api.views import OrderViewSet
+from snap_buy.payments.api.views import PaymentViewSet
 from snap_buy.products.api.views import ProductCategoryViewSet
 from snap_buy.products.api.views import ProductViewSet
+from snap_buy.users.api.views import AddressViewSet
 from snap_buy.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
-router.register("product-categories", ProductCategoryViewSet)
+router.register("users", AddressViewSet, basename="user-address")
+
+
+router.register("products", ProductCategoryViewSet, basename="product-category")
 router.register("products", ProductViewSet)
+
 router.register("orders", OrderViewSet)
-router.register("orders/(?P<order_id>[^/.]+)/items", OrderItemViewSet)
+router.register("orders", OrderItemViewSet, basename="order-orderitem")
+
+router.register("payments", PaymentViewSet, basename="order-payment")
+
 
 app_name = "api"
 urlpatterns = router.urls
