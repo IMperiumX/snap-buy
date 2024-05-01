0ee3ac9f99b214f4a5b9fe8191a12896ab52e9fc
IMperiumX <yusufadell.dev@gmail.com>
2024-05-01 11:48:07 +0300

    Add products api endpoints

--- config/api_router.py
+++ config/api_router.py
@@ -2,12 +2,15 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
+from snap_buy.products.api.views import ProductCategoryViewSet
+from snap_buy.products.api.views import ProductViewSet
 from snap_buy.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
-
+router.register("product-categories", ProductCategoryViewSet)
+router.register("products", ProductViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
